6117bc85ed59bba2708880e09ac896250ecae13e
Sebastião Soares da Silva Filho <123883250+seb2301@users.noreply.github.com>
2025-05-19 20:55:38 -0300

    Delete pokedex directory

--- pokedex/pokedex/src/main/java/com/example/pokedex/repository/PokemonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.pokedex.repository;
-
-import com.example.pokedex.model.Pokemon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PokemonRepository extends JpaRepository<Pokemon, String> {
-}
